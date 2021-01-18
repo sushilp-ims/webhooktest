8a8c8255f64b105a7b9e1b3fbff9c9eada60fe50
John Doe <johndoe@example.com>
2021-01-18 16:13:31 +0530

    zff

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ function callSendAPI(sender_psid, response) {
       recipient: {
           id: sender_psid
       },
-      message: response + ' ' +String(sender_psid)
+      message: response 
   };
 
   // console.log(request_body);
